7c0a934391ee0c289ed5a05e3880716f8db7bfd3
Andrew Silva <andrewsilva9@gmail.com>
2019-05-09 02:05:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,6 +27,10 @@ class Song
   def self.find_by_name(name)
     @@all.find {|person| person.name == name}
   end
-  def self.find_or_create_by_name
+  def self.find_or_create_by_name(name)
+    if @@all.find {|person| person.name == name}
+      name
+    else
+      self.create
   end
 end
